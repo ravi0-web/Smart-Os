b64462136869f0000c38d943216e6ace7b007f10
ravi0-web <ravi.01022004g@gmail.com>
2025-11-04 11:27:23 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('cpu_scheduler/', views.cpu, name='cpu_scheduler'),
     path('pager/', views.paging, name='pager'),
  path('sjf/', views.sjf, name='sjf'),
+    path("chatbot_api/", views.chatbot_api, name="chatbot_api"),
   
    
     
